3fc6da14e456fce95356847f3acf27af16612f33
Annesley@codecommit-at-789734661575 <Annesley@codecommit-at-789734661575>
2024-11-21 17:10:20 +0530

    changes

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -4,20 +4,20 @@ variable "lambda_functions" {
     {
       function_name = "g2_handler"
       runtime       = "python3.9"
-    #   role_arn      = "arn:aws:iam::123456789012:role/lambda-execution-role"
-      handler       = "lambda_handler"
-      filename      = "../g2_handler/lambda_function.zip"
-      environment   = { ENV = "prod", DEBUG = "false" }
-      tags          = { Name = "g2_handler" }
+      #   role_arn      = "arn:aws:iam::123456789012:role/lambda-execution-role"
+      handler     = "lambda_handler"
+      filename    = "../extract/g2_handler/lambda_function.zip"
+      environment = { ENV = "prod", DEBUG = "false" }
+      tags        = { Name = "g2_handler" }
     },
     {
       function_name = "gamil_extractor"
       runtime       = "python3.9"
-    #   role_arn      = "arn:aws:iam::123456789012:role/lambda-execution-role"
-      handler       = "lambda_handler"
-      filename      = "../gamil_extractor/lambda_function.zip"
-      environment   = { ENV = "staging", DEBUG = "true" }
-      tags          = { Name = "gamil_extractor" }
+      #   role_arn      = "arn:aws:iam::123456789012:role/lambda-execution-role"
+      handler     = "lambda_handler"
+      filename    = "../extract/gamil_extractor/lambda_function.zip"
+      environment = { ENV = "staging", DEBUG = "true" }
+      tags        = { Name = "gamil_extractor" }
     }
   ]
 }
