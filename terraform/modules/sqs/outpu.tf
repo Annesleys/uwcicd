84e9107124b0b595473e44b19894efcd611f53fd
Annesley@codecommit-at-789734661575 <Annesley@codecommit-at-789734661575>
2024-11-22 12:46:54 +0530

    changes

--- terraform/modules/sqs/outpu.tf
+++ terraform/modules/sqs/outpu.tf
@@ -1,7 +1,7 @@
 output "queue_url" {
-  value = aws_sqs_queue.my_queue.id
+  value = aws_sqs_queue.sqs
 }
 
 output "queue_arn" {
-  value = aws_sqs_queue.my_queue.arn
+  value = aws_sqs_queue.sqs.arn
 }
