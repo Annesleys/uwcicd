6b07ca0a5bf32f2d216f757ca381db040e1e0bab
Annesley@codecommit-at-789734661575 <Annesley@codecommit-at-789734661575>
2024-11-22 09:46:48 +0530

    changes

--- extract/gamil_extractor/lambda_function.py
+++ extract/gamil_extractor/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Gmail Extractor!')
+        'body': json.dumps('Gmail Extractor V2!')
     }
