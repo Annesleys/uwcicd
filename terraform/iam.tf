dc99c948db558ecb77c6ea75a57e2c7b38867a80
Annesley@codecommit-at-789734661575 <Annesley@codecommit-at-789734661575>
2024-11-22 12:26:40 +0530

    changes

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,5 +1,7 @@
-resource "aws_iam_role" "lambda_execution_role" {
-  name               = "lambda_execution_role_uw"
+################## g2 Handler IAM Role ####################
+
+resource "aws_iam_role" "lambda_execution_role_g2_handler" {
+  name               = "lambda_execution_role_g2_handler"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -14,8 +16,8 @@ resource "aws_iam_role" "lambda_execution_role" {
   })
 }
 
-resource "aws_iam_policy" "lambda_execution_policy" {
-  name   = "lambda_execution_policy_uw"
+resource "aws_iam_policy" "lambda_execution_policy_g2_handler" {
+  name   = "lambda_execution_policy_g2_handler"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -33,11 +35,59 @@ resource "aws_iam_policy" "lambda_execution_policy" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_execution_policy_attachment" {
-  role       = aws_iam_role.lambda_execution_role.name
-  policy_arn = aws_iam_policy.lambda_execution_policy.arn
+resource "aws_iam_role_policy_attachment" "lambda_execution_policy_g2_handler_attachment" {
+  role       = aws_iam_role.lambda_execution_role_g2_handler.name
+  policy_arn = aws_iam_policy.lambda_execution_policy_g2_handler.arn
 }
 
 output "role_arn" {
-  value = aws_iam_role.lambda_execution_role.arn
+  value = aws_iam_role.lambda_execution_role_g2_handler.arn
 }
+
+################## Gmail Extractor IAM Role ####################
+
+resource "aws_iam_role" "lambda_execution_role_gmail_Extractor" {
+  name               = "lambda_execution_role_gmail_Extractor"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "lambda_execution_policy_gmail_Extractor" {
+  name   = "lambda_execution_policy_gmail_Extractor"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "s3:GetObject"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_execution_policy_gmail_Extractor_attachment" {
+  role       = aws_iam_role.lambda_execution_role_gmail_Extractor.name
+  policy_arn = aws_iam_policy.lambda_execution_policy_gmail_Extractor.arn
+}
+
+output "role_arn" {
+  value = aws_iam_role.lambda_execution_role_gmail_Extractor.arn
+}
+
+################## add new IAM Role, Policy,  attachments and output ####################
--- terraform/main.tf
+++ terraform/main.tf
@@ -11,26 +11,28 @@ provider "aws" {
   region = "ap-southeast-2" # Change to your desired region
 }
 
-# Loop through the Lambda functions
+# g2 handler Lambda functions
 module "lambda_functions_g2_handler" {
 
   source = "./modules/lambda"
 
   function_name        = "g2_handler"
   runtime              = var.runtime
-  role_arn             = aws_iam_role.lambda_execution_role.arn
+  role_arn             = aws_iam_role.lambda_execution_role_g2_handler.arn
   handler              = var.handler
   filename             = "../extract/g2_handler/lambda_function.zip"
 }
 
+# gmail extractor Lambda functions
 module "lambda_functions_gamil_extractor" {
 
   source = "./modules/lambda"
 
   function_name        = "gamil_extractor"
   runtime              = var.runtime
-  role_arn             = aws_iam_role.lambda_execution_role.arn
+  role_arn             = aws_iam_role.lambda_execution_role_gmail_Extractor.arn
   handler              = var.handler
   filename             = "../extract/gamil_extractor/lambda_function.zip"
 }
 
+# add new lambda modules to create new lambda functions
